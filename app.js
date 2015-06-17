5928622f0abd7aa1df887cb5540054339767fba6
Chad Miller <wick42@gmail.com>
2015-06-16 21:17:59 -0700

    Made it work

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 
-$(function(){
-   $( '#list' ).click(function(){
-   $('.menu').toggleClass('showMenu')
-   });
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+// Show menu
+
+$('#li').click (function() {
+   // alert("click!!!");
+   $( '#menu' ).removeClass('hidden');
+   $( '#menu' ).addClass('showMenu');
+   $( '.articles' ).addClass('articlesShift');
+});
+
+// Hide menu on list click
+
+$('#menu a').click (function() {
+   // alert("click!!!");
+   $( '#menu' ).addClass('hidden');
+   $( '#menu' ).removeClass('showMenu');
+   $( '.articles' ).removeClass('articlesShift');
 });
